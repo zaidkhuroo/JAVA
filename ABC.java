703fa9995080ed68b86b81aea4ba0af5bf2dd457
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2023-05-16 23:33:30 +0530

    Add files via upload

---
+++ ABC.java
@@ -0,0 +1,13 @@
+public class ABC{
+    int a=10;
+    static int b=20;
+    public static void main(String args[])
+    {
+        ABC ob1= new ABC();
+        ob1.a=100;
+        ob1.b=200;
+
+         System.out.print(ob1.a);
+         System.out.print(ob1.b);
+    }
+}
