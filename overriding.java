5881f01493601f36412eadf14ff50dbbb3c0f4dd
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2023-05-17 18:11:49 +0530

    Add files via upload

---
+++ overriding.java
@@ -0,0 +1,19 @@
+class A {
+    public void hello() {
+        System.out.println("hello");
+    }
+}
+    class B extends A
+    {
+        public void hello(){
+            System.out.println("im out");
+        }
+    }
+    
+
+    public class overriding {
+        public static void main(String args[]) {
+            B obj = new B();
+            obj.hello();
+        }
+}
