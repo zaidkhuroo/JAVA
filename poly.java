6b4498a2a9f35fadaf1ca0d53c0baec045131b07
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2023-05-17 20:45:25 +0530

    Add files via upload

---
+++ poly.java
@@ -0,0 +1,38 @@
+// program to demonstrate polymorphisim
+class A {
+    void show() {
+        System.out.println("in A");
+    }
+}
+
+class B extends A {
+    void show() {
+        System.out.println("in B");
+    }
+}
+
+class C extends B {
+    void show() {
+        System.out.println("in C");
+    }
+}
+
+public class poly {
+    public static void main(String args[]) {
+        A obj = new A(); // creating object
+        obj.show(); // calling method
+
+        obj = new B(); // here we are assigning a new object to the old variable and thus calling the
+                       // show method of B
+        obj.show();
+
+        obj = new C(); // here we are assigning a new object to the old variable and thus calling the
+                       // show method of C
+        obj.show();
+    }
+}
+
+// OUTPUT:
+// in A
+// in B
+// in C
