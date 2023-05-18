815270e06edc9d49f37231dbb87a847812365e55
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2023-05-18 13:31:20 +0530

    Add files via upload

---
+++ trycatch.java
@@ -0,0 +1,17 @@
+import java.net.SocketPermission; //library imports automatically once the exception keyword is invoked
+
+public class trycatch {
+
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 0;
+        int t = 0;
+
+        try { //applying try method on integer when divided with b(0)
+            t = a / b;
+        } catch (Exception e) { //catches the exception and prints the result
+           System.out.println("cannot divide by zero"); 
+        }
+        System.out.println("Exception: " + t);
+    }
+}
