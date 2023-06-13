6d8892821b8e45c3bf3907428d20c190ed34a713
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2023-06-13 23:39:09 +0530

    Update interfaces.java

--- interfaces.java
+++ interfaces.java
@@ -2,7 +2,7 @@ interface ABC { //1st interface
      void prints();
      public static final int a=10; //a variable must contain  public static final kyword with it in interface
 }
-interface ABCD { //2nd interface
+interface ABCD { //2nd interface 
     void disp(); 
     public static final int a=10; //a variable must contain  public static final kyword with it in interface
 }
