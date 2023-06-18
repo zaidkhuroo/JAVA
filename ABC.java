8cafbfeffe1ec483a07b92df6c72ef2ced532686
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2023-06-18 22:53:53 +0530

    Update ABC.java

--- ABC.java
+++ ABC.java
@@ -4,7 +4,7 @@ public class ABC{
     public static void main(String args[])
     { 
         ABC ob1= new ABC();
-        ob1.a=100;
+        ob1.a=100; 
         ob1.b=200;
 
          System.out.print(ob1.a);
